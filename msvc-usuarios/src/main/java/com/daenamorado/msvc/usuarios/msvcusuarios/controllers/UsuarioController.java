ea667097180773d2f6d52fdb1f3fb6a430e2facb
David Enamorado <daenamorado@bancatlan.hn>
2024-02-28 10:45:44 -0600

    feat(usuario): put and post user methods added

--- msvc-usuarios/src/main/java/com/daenamorado/msvc/usuarios/msvcusuarios/controllers/UsuarioController.java
+++ msvc-usuarios/src/main/java/com/daenamorado/msvc/usuarios/msvcusuarios/controllers/UsuarioController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,11 @@ import com.daenamorado.msvc.usuarios.msvcusuarios.models.entity.usuario;
 import com.daenamorado.msvc.usuarios.msvcusuarios.services.UsuarioService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @RestController()
@@ -25,10 +31,38 @@ public class UsuarioController {
     }
 
 
+    /**
+     * @param id
+     * @return
+     */
     @GetMapping("/{id}")
     public ResponseEntity<?> getById(@PathVariable Long id) { 
         Optional<usuario> optionalUsuario = service.getById(id);
         return (optionalUsuario.isPresent()) ? ResponseEntity.ok(optionalUsuario.get()) : ResponseEntity.notFound().build();
+    }
+
+    /**
+     * @param usuario
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity<usuario> create(@RequestBody usuario usuario) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuario));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody usuario usuario, @PathVariable Long id) {
+        Optional<usuario> o = service.getById(id);
+
+        if(o.isPresent()) {
+            usuario usuarioDb = o.get();
+            usuarioDb.setName(usuario.getName());
+            usuarioDb.setEmail(usuario.getEmail());
+            usuarioDb.setPassword(usuario.getPassword());
+            return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuarioDb));
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
 }
