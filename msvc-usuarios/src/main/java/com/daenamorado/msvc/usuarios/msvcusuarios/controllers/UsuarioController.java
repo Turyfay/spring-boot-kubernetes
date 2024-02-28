49f13c2fcdbb7bede7036eae4e2da4c0bf2a2fa4
David Enamorado <daenamorado@bancatlan.hn>
2024-02-28 13:50:10 -0600

    chore: changing configuration properties

--- msvc-usuarios/src/main/java/com/daenamorado/msvc/usuarios/msvcusuarios/controllers/UsuarioController.java
+++ msvc-usuarios/src/main/java/com/daenamorado/msvc/usuarios/msvcusuarios/controllers/UsuarioController.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +17,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 
 @RestController()
 public class UsuarioController {
